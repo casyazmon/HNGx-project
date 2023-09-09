001840f5b953819537fa248f79df77fb68257369
casyazmon <casyazmon@gmail.com>
2023-09-09 23:32:20 +0100

    initial commit

---
+++ StageOneTask/src/main/java/com/casyazmon/StageOneTask/StageOneTaskApplication.java
@@ -0,0 +1,13 @@
+package com.casyazmon.StageOneTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageOneTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageOneTaskApplication.class, args);
+	}
+
+}
---
+++ StageOneTask/src/test/java/com/casyazmon/StageOneTask/StageOneTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casyazmon.StageOneTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageOneTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
